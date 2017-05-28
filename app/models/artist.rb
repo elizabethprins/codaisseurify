b86037b808deb294088aa5697e5adfc108095532
elizabethprins <elizabethprins@gmail.com>
2017-05-28 19:51:16 +0200

    add some styling

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -3,4 +3,12 @@ class Artist < ApplicationRecord
 
   validates :name, :img_url, presence: true, uniqueness: true
   validates :img_url, length: { maximum: 255 }, format: URI::regexp(%w(http https))
+
+  def self.order_by_name
+    order(:name)
+  end
+
+  def self.order_by_date
+    order(:date)
+  end
 end
